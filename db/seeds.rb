ca25fbe63e70e8bd1daa67ce37b4ff52648505e5
Mark Camilleri <markcamilleri10@gmail.com>
2020-09-22 22:37:08 -0400

    Seeded demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+demo = User.create(username: "demo_user", email: "demo@blackbird.com", 
+  first_name: "Demo", last_name: "User", password: "hunter12")
