0cb310f10e3a60f53c9fb491731611eaedca90bf
Mark Camilleri <markcamilleri10@gmail.com>
2020-10-01 16:28:43 -0400

    Buy Sell btn nearly done

--- frontend/components/company/company_sidebar/company_sidebar.jsx
+++ frontend/components/company/company_sidebar/company_sidebar.jsx
@@ -7,11 +7,33 @@ export default class CompanySidebar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectValue: 'shares'
+      selectValue: 'shares',
+      sellFormActive: false,
+      activeBuyBtn: true,
+      activeSellBtn: false
     };
+    this.handleBuyBtnClick = this.handleBuyBtnClick.bind(this);
+    this.handleSellBtnClick = this.handleSellBtnClick.bind(this);
     this.handleSelectChange = this.handleSelectChange.bind(this);
   }
 
+  componentDidUpdate() {
+    const { user, company } = this.props;
+    const ownedCompanies = Object.keys(user.shares);
+    if (this.state.sellFormActive === true) {
+      return null;
+    } else if (ownedCompanies.includes(company.symbol)) {
+      this.setState({ sellFormActive: true });
+    }
+  }
+
+  handleBuyBtnClick() {
+    this.setState({ activeBuyBtn: true, activeSellBtn: false });
+  }
+  
+  handleSellBtnClick() {
+    this.setState({ activeBuyBtn: false, activeSellBtn: true });
+  }
 
   handleSelectChange(e) {
     this.setState({ selectValue: e.target.value });
@@ -19,12 +41,26 @@ export default class CompanySidebar extends React.Component {
 
   render() {
     const { user, company, color, createShare, updateUser } = this.props;
-    const { inputValue, selectValue } = this.state;
+    const { selectValue, sellFormActive, activeBuyBtn, activeSellBtn } = this.state;
     if (!company) return null;
     return (
       <div className='company-sidebar'>
         <div className='company-sidebar-title'>
-          <h3>Buy {company.symbol}</h3>
+          {sellFormActive ? (
+            <div className='share-form-title-btns'>
+              <button className='share-form-title-btn'
+                className={activeBuyBtn ? (`buy-btn ${color}-h` + ` ${color}` + `-bb2`) : (`buy-btn pad2 ${color}-h`)}
+                onClick={this.handleBuyBtnClick}
+                >Buy {company.symbol}
+              </button>
+              <button 
+                className={activeSellBtn ? (`sell-btn ${color}-h` + ` ${color}` + `-bb2`) : (`sell-btn pad2 ${color}-h`)}
+                onClick={this.handleSellBtnClick}
+              >Sell {company.symbol}</button>
+            </div>
+          ) : (
+            <h3>Buy {company.symbol}</h3>
+          )}
         </div>
         <div className='share-form-container'>
           <form onSubmit={this.handleSubmit} className='shares-select-form'>
