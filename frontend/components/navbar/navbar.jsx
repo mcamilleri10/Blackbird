9e6f3524e7cd4c6a9b288ee3e920519de760ed1e
Mark Camilleri <markcamilleri10@gmail.com>
2020-10-26 14:01:01 -0400

    Add angellist link

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFeatherAlt } from '@fortawesome/free-solid-svg-icons';
-import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faAngellist } from '@fortawesome/free-brands-svg-icons';
 import Search from './search/search';
 
 
@@ -13,8 +14,9 @@ export default class Navbar extends React.Component {
   render() {
     const logo = <FontAwesomeIcon icon={faFeatherAlt}/>;
     const { searchResults, symbolSearch, color } = this.props;
-    const github = <FontAwesomeIcon icon={faGithubSquare} />;
+    const github = <FontAwesomeIcon icon={faGithub} />;
     const linkedin = <FontAwesomeIcon icon={faLinkedin} />;
+    const angel = <FontAwesomeIcon icon={faAngellist} />;
     return (
       <div className='navbar'>
         <div className='navbar-left'>
@@ -28,6 +30,9 @@ export default class Navbar extends React.Component {
           />
         </div>
         <div className='navbar-right'>
+          <a href="https://angel.co/u/mark-camilleri-1" className={`${color}-h`}>
+            {angel}<span>AngelList</span>
+          </a>
           <a href="https://github.com/troubadour10" className={`${color}-h`}>
             {github}<span>Github</span>
           </a>
